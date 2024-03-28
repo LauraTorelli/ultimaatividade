d1816573b11e0bcf0e1f868bf4ce33e6e9885270
Laura Torelli <125595086+LauraTorelli@users.noreply.github.com>
2024-03-28 15:21:21 -0300

    Create OrderBy.sql

---
+++ OrderBy.sql
@@ -0,0 +1,6 @@
+SELECT * FROM `Alunos` ORDER BY Nome;
+SELECT * FROM `Alunos` ORDER BY ID;
+SELECT * FROM `Alunos` ORDER BY Idade;
+SELECT * FROM `Alunos` ORDER BY Sobrenome;
+SELECT * FROM `Alunos` ORDER BY Cidade;
+SELECT * FROM `Alunos` ORDER BY Estado;
